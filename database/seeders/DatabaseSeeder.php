3c698176b06bcc1d06ff6846a1dcec4af75e5ac8
Camila Ripoll <157710935+Cmila05@users.noreply.github.com>
2025-04-11 19:33:31 -0500

    cambios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Hash;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +18,17 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //    'name' => 'Test User',
+        //    'email' => 'test@example.com',
+        //]);
+
+        DB::table("users")->insert([
+            "name" => "admini",
+            "email" => "admin@example.com",
+            "password" => Hash::make("clave")
+
+
         ]);
     }
 }
